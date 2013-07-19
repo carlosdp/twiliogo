2573ee1fface0ea83a06744622e8c9dd515c5a38
Carlos Diaz-Padron <carlos@carlosdp.io>
2013-07-19 14:36:53 -0700

    Add SendMessage to send texts

--- message.go
+++ message.go
@@ -1,5 +1,10 @@
 package twilio
 
+import (
+  "encoding/json"
+  "net/url"
+)
+
 type Message struct {
   Sid string `json:"sid"`
   DateCreated string `json:"date_created"`
@@ -14,5 +19,25 @@ type Message struct {
   Price string `json:"price"`
   PriceUnit string `json:"price_unit"`
   ApiVersion string `json:"api_version"`
-  uri string `json:"uri"`
+  Uri string `json:"uri"`
+}
+
+func SendMessage(client Client, from string, to string, body string) (*Message, error) {
+  var message *Message
+
+  params := url.Values{}
+  params.Set("From", from)
+  params.Set("To", to)
+  params.Set("Body", body)
+
+  res, err := client.post(params, client.RootUrl() + "/SMS/Messages.json")
+
+  if err != nil {
+    return message, err
+  }
+
+  message = new(Message)
+  err = json.Unmarshal(res, message)
+
+  return message, err
 }
